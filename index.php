100d71280762e36adb2df360ad25fac203043eff
Thales Monteiro <the.monteiro.dev@gmail.com>
2017-05-05 19:36:29 -0300

    Deepin

--- index.php
+++ index.php
@@ -13,17 +13,16 @@
 		<div class="slide2 slide">
 			<div class="overlay">
 				<div class="container">
-					<h3 class="text-title text-center">Bem Vindo a CR Copiadoras</h3>
-					<h4 class="text-subtitle text-center">Nós temos a solução ideal para o seu negócio!</h4>
+					<h3 class="text-title text-center">Não importa o tamanho das suas idéias</h3>
+					<h4 class="text-subtitle text-center">Nós te ajudamos a colocar no papel!</h4>
 				</div>
 			</div>
-
 		</div>
 		<div class="slide3 slide">
 			<div class="overlay">
 				<div class="container">
-					<h3 class="text-title text-center">Bem Vindo a CR Copiadoras</h3>
-					<h4 class="text-subtitle text-center">Nós temos a solução ideal para o seu negócio!</h4>
+					<h3 class="text-title text-center">A gestão de documentos que faltava para sua empresa!</h3>
+					<h4 class="text-subtitle text-center">Para grandes, médias e pequenas empresas!</h4>
 				</div>
 			</div>
 
@@ -31,22 +30,12 @@
 		<div class="slide4 slide">
 			<div class="overlay">
 				<div class="container">
-					<h3 class="text-title text-center">Bem Vindo a CR Copiadoras</h3>
-					<h4 class="text-subtitle text-center">Nós temos a solução ideal para o seu negócio!</h4>
+					<h3 class="text-title text-center">O melhor que a tecnologia pode proporcionar</h3>
+					<h4 class="text-subtitle text-center">Aqui você encontra a tendência e inovação em termos tecnológicos!</h4>
 				</div>
 			</div>
 
 		</div>
-		<div class="slide5 slide">
-			<div class="overlay">
-				<div class="container">
-					<h3 class="text-title text-center">Bem Vindo a CR Copiadoras</h3>
-					<h4 class="text-subtitle text-center">Nós temos a solução ideal para o seu negócio!</h4>
-				</div>
-			</div>
-
-		</div>
-
 	</div>
 </section>
 
