3116b4923a12f31a809570ce6b79c42343ec5805
Thales Monteiro <tmontdev@gmail.com>
2017-08-03 00:05:38 -0300

    going to sleep 02/08/2017

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
   	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/animate.com" media="screen" title="animate" charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<?php wp_head(); ?>
+	<?php wp_head();?>
 </head>
 <body>
 	<header class="header whole-block" id="header">
@@ -27,6 +27,36 @@
 					<span class="page-title text-title">Home</span>
 				</div>
 
+			</div>
+		</section>
+		<section class="header-content whole-block">
+			<div class="row">
+				<div class="header-info-desktop desktop">
+					
+				</div> 
+			</div>
+			<div class="row">
+				<menu class="header-menu whole-block">
+					<nav>
+						<ul class="header-menu-list">
+							<li class="col-xs-12 col-lg-2 header-logo header-link">
+								<a href="#"><span class="fa fa-home link-icon"></span><span class="link-name"> Home</span></a>
+							</li>
+							<li class="col-xs-12 col-lg-2 about-link header-link">
+								<a href="#"><span class="fa fa-question-circle link-icon"></span><span class="link-name"> Sobre</span></a>
+							</li>
+							<li class="col-xs-12 col-lg-2 equipment-link header-link">
+								<a href="#"><span class="fa fa-print link-icon"></span><span class="link-name"> Equipamentos</span></a>
+							</li>
+							<li class="col-xs-12 col-lg-2 services-link header-link">
+								<a href="#"><span class="fa fa-cog link-icon"></span><span class="link-name"> Serviços</span></a>
+							</li>
+							<li class="col-xs-12 col-lg-2 contact-link header-link">
+								<a href="#"><span class="fa fa-phone link-icon"></span><span class="link-name"> Contato</span></a>
+							</li>
+						</ul>
+					</nav>
+				</menu>
 			</div>
 		</section>
 
